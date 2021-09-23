4aced4e99f92b350effb47dc7f721a31e425dbb8
Tomasz Syryczyk <tomasz.syryczyk@gmail.com>
2021-09-23 19:16:43 +0200

    modal test

--- Front/src/Controls/Button/Button.js
+++ Front/src/Controls/Button/Button.js
@@ -3,11 +3,13 @@ import './Button.css';
 
 class Button extends React.Component{
     render(){
-        var props = this.props;
+        
+        var onClick = this.props.onClick;
+        var text = this.props.text;
         debugger;
         return (
             <div className="Button">
-                    
+                <button onClick={onClick} value={text} />        
             </div>
         );
     }
