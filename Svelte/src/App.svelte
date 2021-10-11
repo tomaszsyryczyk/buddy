c967a6afbd8977da102b46bf6436191efa76d0a1
Tomasz Syryczyk(W101825) <Tomasz.Syryczyk@santanderconsumer.dk>
2021-10-11 22:05:03 +0200

    menu controls

--- Svelte/src/App.svelte
+++ Svelte/src/App.svelte
@@ -2,32 +2,32 @@
 	import { Router, Link, Route } from "svelte-navigator";
 	import Home from "./routes/home/home.svelte";
 	import Stocks from "./routes/stocks/stocks.svelte";
+	import MenuItem from "./controls/menuitem/menuitem.svelte";
 
 	function openNav() {
 		var item = document.getElementById("sideNav");
 		if(item)
 			item.style.width = "250px";
-  		// document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+  		document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 	}
 
 	function closeNav() {		
-		var item = document.getElementById("sideNav");
+		var item = document.getElementById("sideNav");	
 		if(item)
 			item.style.width = "0";
-  		// document.body.style.backgroundColor = "white";
+  		document.body.style.backgroundColor = "white";
 	}
 </script>
 
 <Router>
 	<nav id="sideNav" class="navigation">		
-		<button class="closebtn" on:click="{closeNav}">&times;</button>
-		<ul>
-			<li><Link on:click="{closeNav}" to="/">Home</Link></li>
-			<li><Link on:click="{closeNav}" to="/stocks">Stocks</Link></li>			
-		</ul>
+		<button class="closebtn" on:click="{closeNav}">&times;</button>	
+		<MenuItem click="{closeNav}" url="/" text="Home"/>
+		<MenuItem click="{closeNav}" url="/stocks" text="Stocks"/>
+		<MenuItem click="{closeNav}" url="/" text="nowy element listy dupa test"/>			
 	</nav>
 	<div class="content">
-		<button on:click="{openNav}" >TEST</button>
+		<button on:click="{openNav}" >&#9776;</button>
 		<Route path="/">
 			<Home />
 		</Route>
---
+++ Svelte/src/controls/menuitem/menuitem.svelte
@@ -0,0 +1,16 @@
+<script>    
+	import { Router, Route, Link } from "svelte-navigator";
+    export let url;
+    export let click;
+    export let text;
+
+    function safeFunction(){
+        if(click)
+            click();
+    }
+</script>
+
+<Link to="{url}" on:click="{safeFunction}" >
+    <div class="icon"></div>
+    <div class="content">{text}</div>
+</Link>
