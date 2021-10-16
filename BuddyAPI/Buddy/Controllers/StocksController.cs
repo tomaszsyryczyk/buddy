ccbff9bca3730a0215070ac3bb0f23a5bf00cc23
Tomasz Syryczyk <tomasz.syryczyk@gmail.com>
2021-10-17 01:58:15 +0200

    working swagger

---
+++ BuddyAPI/Buddy/Controllers/StocksController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Buddy.Controllers
+{
+    [ApiController]
+    [Route("api/stocks")]
+    public class StocksController
+    {
+
+        [HttpGet("current")]
+        public string Current()
+        {
+            return "test";
+        }
+
+        [HttpGet("history")]
+        public void History()
+        {
+        }
+    }
+}
--- BuddyAPI/Buddy/Startup.cs
+++ BuddyAPI/Buddy/Startup.cs
@@ -1,15 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 
 namespace Buddy
 {
@@ -26,6 +20,12 @@ namespace Buddy
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+            });
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -42,9 +42,17 @@ namespace Buddy
 
             app.UseAuthorization();
 
+           
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapControllers();
+            });
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
             });
         }
     }
