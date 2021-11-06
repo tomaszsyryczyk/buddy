9bee88f1b8bf486be1a4d166d521273169889bde
Tomasz Syryczyk <tomasz.syryczyk@gmail.com>
2021-11-06 22:01:42 +0100

    installer for electron

--- Svelte/start-electron.js
+++ Svelte/start-electron.js
@@ -1,6 +1,7 @@
 const { app, BrowserWindow, screen } = require('electron');
 require('electron-reload')(__dirname);
 
+
 const createWindow = () => {
     const { width, height } = screen.getPrimaryDisplay().workAreaSize;
 
