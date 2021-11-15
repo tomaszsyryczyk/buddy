0bfc07e6b5b4bfefebb3b611794b441f061a7b13
tsyryczyk <tomasz.syryczyk@gmail.com>
2021-11-15 19:46:42 +0100

    adding table

--- Svelte/src/controls/table/table.svelte
+++ Svelte/src/controls/table/table.svelte
@@ -3,24 +3,22 @@
     export let rowMappers = [];
 </script>
 
-{#each data as row}
-    {#each rowMappers as rowMapper}
-                name: {rowMapper.name} | value: {rowMapper.getValue(row)} <br/>
-            {/each}   
-           {/each}   
-<!-- <div class="row">
-             <div class="col-xs-offset-11 col-xs-1">
-                     <div class="box btn">
-                          <Button click={toggleModal} icon="fas fa-plus" />
-                        </div>
-                   </div>
-              </div>
-          <div class="table">
-          <Header values="{columnNames}"/>     
-           {#each currentStocks as row}
-              <Row data="{row}" mapper={mapper} />
-           {/each}   
-        </div> -->
+<div class="table">
+    <div class="header">
+        {#each rowMappers as header}
+            header name: {header.name}
+        {/each}
+    </div>
+    <div class="content">
+        {#each data as row}
+            {#each rowMappers as getter}
+                name: {getter.getValue()}
+            {/each}
+        {/each}
+    </div>
+asdf
+    <div class="footer"></div>
+</div>
 <style>
 
 </style>
--- Svelte/src/routes/stocks/current/current.svelte
+++ Svelte/src/routes/stocks/current/current.svelte
@@ -1,18 +1,15 @@
 <script>    
     import { onMount } from "svelte";
-    import Header from "./currentheader.svelte";
-    import Row from "./currentrow.svelte";
     import Stocks from "./../stocks.svelte";
-    import Button from "./../../../controls/button/button.svelte";
     import AddStockModal from "./../addStockModal/addStockModal.svelte";
     import Table from "./../../../controls/table/table.svelte";
     let currentStocks = [];
 
-    let columnNames = ['Name', 'Ilość', 'zł/akcja', 'Marża', 'Data', 'na 0', 'Aktualna', '%', 'Sprzedaż', 'Zysk'];
+    //let columnNames = ['Name', 'Ilość', 'zł/akcja', 'Marża', 'Data', 'na 0', 'Aktualna', '%', 'Sprzedaż', 'Zysk'];
 
-    function mapper(data){
-        return [data.name, data.count,data.price, data.fee, data.data, data.equal, data.current,data.delta, data.sell, data.income];
-    };
+    // function mapper(data){
+    //     return [data.name, data.count,data.price, data.fee, data.data, data.equal, data.current,data.delta, data.sell, data.income];
+    // };
 
     onMount(async () => {
         axios('https://localhost:5001/api/stocks/current')
@@ -22,10 +19,6 @@
     })
 
     let modalOpen = false;
-
-    function toggleModal(){
-        modalOpen = !modalOpen;
-    }
 
     function rowMapper(){
         return [
@@ -54,19 +47,5 @@
 
 
 <style>
-  .table {
-  display: flex;
-  flex-flow: column nowrap;
-  font-size: .8rem;
-  margin: 0.5rem;
-  line-height: 1.5;
-  border-bottom: 1px solid #d0d0d0;
-  flex: 1 1 auto;
-  }
-
- .btn {
-        justify-content: flex-start;
-    }
-
 
 </style>
