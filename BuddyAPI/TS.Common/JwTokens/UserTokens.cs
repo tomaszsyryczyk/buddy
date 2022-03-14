e35044a3dcd7d994f2b632d45fa2bd630ecf113d
Tomasz Syryczyk <tomasz.syryczyk@gmail.com>
2022-03-14 22:29:38 +0100

    jwt

---
+++ BuddyAPI/TS.Common/JwTokens/UserTokens.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace TS.Common.JwTokens
+{
+    public class UserTokens
+    {
+        public string Token
+        {
+            get;
+            set;
+        }
+        public string UserName
+        {
+            get;
+            set;
+        }
+        public TimeSpan Validaty
+        {
+            get;
+            set;
+        }
+        public string RefreshToken
+        {
+            get;
+            set;
+        }
+        public Guid Id
+        {
+            get;
+            set;
+        }
+        public string EmailId
+        {
+            get;
+            set;
+        }
+        public Guid GuidId
+        {
+            get;
+            set;
+        }
+        public DateTime ExpiredTime
+        {
+            get;
+            set;
+        }
+    }
+}
